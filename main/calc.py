df88b806e3bfc950c2c5f7bb2d0a0f02e6551c9c
jbalan7576 <jbalan_7576@yahoo.com>
2018-03-07 14:25:42 +0000

    This the the 3rd commit post moving the code to a subdirectory called main

---
+++ main/calc.py
@@ -0,0 +1,21 @@
+calc=input("Please enter the digits and operator for calculation: ")
+num1='0'
+num2='0'
+flag='Gotonum1'
+for a in calc:
+  if a in ['0','1','2','3','4','5','6','7','8','9']:
+      if flag=='Gotonum1':
+             num1= num1 + str(a)
+      else:
+             num2=num2+str(a)
+  elif a in ['+', '-', '*', '/']:
+        operator=a
+        flag='Gotonum2'
+        continue
+print (num1) 
+print (num2)
+if  operator == '+': print (int(num1) + int(num2))
+elif operator == '-': print (int(num1) - int(num2))
+elif operator == '*': print (int(num1) * int(num2))
+elif operator == '/': print (int(num1) / int(num2))
+else: print('You have selected an invalid operator, please select +, -, * or / only')
